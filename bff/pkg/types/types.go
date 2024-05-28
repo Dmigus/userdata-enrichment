27a849afe2820319b57b73b3b0d8ff1e805b4df5
Dmigus <44640095+Dmigus@users.noreply.github.com>
2024-05-28 10:07:52 +0300

    Реализована обработка в enricher

--- bff/pkg/types/types.go
+++ bff/pkg/types/types.go
@@ -1,11 +1,27 @@
 package types
 
+import "go.uber.org/zap"
+
 type Age = int
 type Sex = string
 type Nationality = string
 
 type FIO struct {
 	name, surname, patronymic string
+}
+
+func (fio FIO) hasPatronymic() bool {
+	return len(fio.patronymic) > 0
+}
+
+func FioToZaFields(fio FIO) []zap.Field {
+	fs := make([]zap.Field, 0, 3)
+	fs = append(fs, zap.String("name", fio.name))
+	fs = append(fs, zap.String("surname", fio.surname))
+	if fio.hasPatronymic() {
+		fs = append(fs, zap.String("patronymic", fio.patronymic))
+	}
+	return fs
 }
 
 type EnrichedRecord struct {
--- enricher/internal/service/enricher.go
+++ enricher/internal/service/enricher.go
@@ -3,6 +3,8 @@ package service
 import (
 	"bff/pkg/types"
 	"context"
+	"enricher/internal/providers/messagehandler"
+	"go.uber.org/zap"
 )
 
 type (
@@ -14,21 +16,38 @@ type (
 		Run(context.Context, Handler) error
 	}
 	repository interface {
+		Store(ctx context.Context, rec types.EnrichedRecord) error
 	}
 	EnrichService struct {
-		runner fioHandlingRunner
+		enricher messagehandler.Enricher
+		runner   fioHandlingRunner
+		logger   *zap.Logger
+		repo     repository
 	}
 )
 
-func NewEnrichService(runner fioHandlingRunner) *EnrichService {
-	return &EnrichService{runner: runner}
+func NewEnrichService(runner fioHandlingRunner, enricher messagehandler.Enricher, logger *zap.Logger, repo repository) *EnrichService {
+	return &EnrichService{runner: runner, enricher: enricher, logger: logger, repo: repo}
 }
 
 func (en *EnrichService) Run(ctx context.Context) error {
 	var handleFIOScenario funcHandler = func(ctx context.Context, fio types.FIO) {
-
+		enriched, err := en.enricher.Enrich(ctx, fio)
+		if err != nil {
+			en.handleErr(ctx, fio, "err enriching fio", err)
+			return
+		}
+		err = en.repo.Store(ctx, enriched)
+		en.handleErr(ctx, fio, "err storing to repository", err)
 	}
 	return en.runner.Run(ctx, handleFIOScenario)
+}
+
+func (en *EnrichService) handleErr(ctx context.Context, fio types.FIO, msg string, err error) {
+	if err != nil {
+		fields := append(types.FioToZaFields(fio), zap.Error(err))
+		en.logger.Error(msg, fields...)
+	}
 }
 
 func (fh funcHandler) Handle(ctx context.Context, fio types.FIO) {
